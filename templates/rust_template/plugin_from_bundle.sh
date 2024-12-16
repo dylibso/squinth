fd8162a4ff4ab5243d4bc5d25b296f4ae2f3a646
dp-mason <davidpattenmason@gmail.com>
2024-12-15 17:02:02 -0700

    update build scripts

---
+++ templates/rust_template/plugin_from_bundle.sh
@@ -0,0 +1,3 @@
+rm -rf ./rust_plugin;
+xtp plugin init --path ./rust_plugin --extension-point ext_01j9hn0bshfh2shvdxk9xz7zys -t ./bundle_v0 -y && \
+./rebuild;
--- templates/rust_template/rebuild.sh
+++ templates/rust_template/rebuild.sh
@@ -1,6 +1,4 @@
 #cargo build --target wasm32-unknown-unknown --release
 
-rm -rf ./rust_plugin;
-xtp plugin init --path ./rust_plugin --extension-point ext_01j9hn0bshfh2shvdxk9xz7zys -t ./bundle_v0 -y && \
 (cd rust_plugin && xtp plugin build);
 # xtp plugin push in order to push up to the xtp instance
