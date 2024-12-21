fa75afe5f9850184e81d85006df788bd9d024b26
dp-mason <davidpattenmason@gmail.com>
2024-12-20 17:37:20 -0700

    rust now compiles witht he custom binding and schema, needs work still though

--- templates/rust_template/plugin_from_bundle.sh
+++ templates/rust_template/plugin_from_bundle.sh
@@ -1,3 +1,7 @@
 rm -rf ./rust_plugin;
-xtp plugin init --path ./rust_plugin --extension-point ext_01j9hn0bshfh2shvdxk9xz7zys -t ./bundle_v0 -y && \
+xtp plugin init \
+    --path ./rust_plugin \
+    --extension-point ext_01j9hn0bshfh2shvdxk9xz7zys \
+    --feature stub-with-code-samples \
+    -t ./bundle_v0 -y && \
 ./rebuild;
