3b7e320d5d5953edc1c6f4fba2ea6238d1c08114
dp-mason <davidpattenmason@gmail.com>
2024-12-22 21:04:03 -0700

    update rebuild script to accommodate xtp integration

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -1,19 +1,19 @@
 rm vcv_module/res/*.wasm;
 
-(cd templates/rust_template; ./rebuild.sh;);
+(cd templates/rust_template && ./rebuild.sh;) || true;
 cp templates/rust_template/rust_plugin/target/wasm32-unknown-unknown/release/plugin.wasm vcv_module/res/rust_plugin.wasm;
 
-(cd templates/js_template; extism-js -i js_template.d.ts js_template.js -o js_template.wasm;);
-cp templates/js_template/js_template.wasm vcv_module/res;
+#(cd templates/js_template; extism-js -i js_template.d.ts js_template.js -o js_template.wasm;);
+#cp templates/js_template/js_template.wasm vcv_module/res;
 
-(cd templates/go_template; tinygo build -target wasi -o go_template.wasm .);
-cp templates/go_template/go_template.wasm vcv_module/res;
+(cd templates/go_template && ./rebuild.sh) || true;
+cp templates/go_template/go_plugin/dist/plugin.wasm vcv_module/res/go_plugin.wasm;
 
-(cd templates/zig_template; zig-build);
-cp templates/zig_template/zig-out/bin/plugin.wasm vcv_module/res/zig_template.wasm;
+#(cd templates/zig_template; zig-build);
+#cp templates/zig_template/zig-out/bin/plugin.wasm vcv_module/res/zig_template.wasm;
 
 # Use the "clang" that comes with Wasi-SDK package, set it in your $PATH
-(cd templates/c_template; clang -o c_template.wasm --target=wasm32-wasi -Wl,--no-entry c_template.c);
-cp templates/c_template/c_template.wasm vcv_module/res;
+#(cd templates/c_template; clang -o c_template.wasm --target=wasm32-wasi -Wl,--no-entry c_template.c);
+#cp templates/c_template/c_template.wasm vcv_module/res;
 
 (cd vcv_module; ./dev_build_and_install.sh;);
