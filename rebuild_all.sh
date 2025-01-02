68d12b64565c7ec312e349e4b00b8f15da0b4ea4
dp-mason <davidpattenmason@gmail.com>
2025-01-01 18:03:13 -0600

    added missing files

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -12,8 +12,7 @@ cp templates/go_template/go_plugin/dist/plugin.wasm vcv_module/res/go_plugin.was
 (cd templates/zig_template && ./rebuild.sh);
 cp templates/zig_template/zig_plugin/zig-out/bin/plugin.wasm vcv_module/res/zig_plugin.wasm;
 
-# Use the "clang" that comes with Wasi-SDK package, set it in your $PATH
-#(cd templates/c_template; clang -o c_template.wasm --target=wasm32-wasi -Wl,--no-entry c_template.c);
-#cp templates/c_template/c_template.wasm vcv_module/res;
+(cd templates/cpp_template && ./rebuild.sh);
+cp templates/cpp_template/cpp_plugin/dist/plugin.wasm vcv_module/res/cpp_plugin.wasm;
 
 (cd vcv_module; ./dev_build_and_install.sh;);
