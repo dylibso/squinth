bf98a41f3a03880c093a06c8896f997ff7a1a9ab
dp-mason <davidpattenmason@gmail.com>
2025-02-14 01:30:09 -0700

    updates to templates for stereo

--- get_prereqs.sh
+++ get_prereqs.sh
@@ -1,5 +1,5 @@
-git clone git@github.com:extism/extism.git extism && \
-(cd extism && make && sudo make install DEST=/usr/local)
+#git clone git@github.com:extism/extism.git extism && \
+#(cd extism && make && sudo make install DEST=/usr/local)
 
 curl https://static.dylibso.com/cli/install.sh -s | bash
 
--- templates/python_template/rebuild.sh
+++ templates/python_template/rebuild.sh
@@ -1,1 +1,1 @@
-(cd py_plugin && xtp plugin build);
+(PYTHONPATH=$(python3 -c "import sys; print(sys.path[-1])") && cd py_plugin && xtp plugin build);
