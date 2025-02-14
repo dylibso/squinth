d54c5f12d0ac06a263080224a42669a79e185391
dp-mason <davidpattenmason@gmail.com>
2025-02-13 21:57:48 -0700

    get prereqs script now constructs all the plugins from bundle

--- get_prereqs.sh
+++ get_prereqs.sh
@@ -2,3 +2,9 @@ git clone git@github.com:extism/extism.git extism && \
 (cd extism && make && sudo make install DEST=/usr/local)
 
 curl https://static.dylibso.com/cli/install.sh -s | bash
+
+for dir in ./templates/*     # list directories in the form "/tmp/dirname/"
+do
+    echo "\n\n\n"${dir}"\n\n\n"
+    (cd ${dir} && ./plugin_from_bundle.sh)
+done
