7e43d7ddbd4a0c4f0c2feec2d1aa89bba2483144
dp-mason <davidpattenmason@gmail.com>
2025-04-29 02:16:09 -0600

    fix noise logic

--- templates/go_template/go_pink_noise/main.go
+++ templates/go_template/go_pink_noise/main.go
@@ -3,9 +3,6 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"math"
 	"math/rand/v2"
 	"unsafe"
 
@@ -16,53 +13,51 @@ func BatchComputeWf() (int32, error) {
 	// BatchComputeWF processes the input buffer and outputs the user-defined wave
 	input := pdk.Input()
 	// _ = input
-	if len(input)%4 != 0 { // || len(input)/4 != 7 {
-		return -1, errors.New(fmt.Sprintf("Improper Input Size, %d", len(input)))
-	}
+	// if len(input)%4 != 0 { // || len(input)/4 != 7 {
+	// 	return -1, errors.New(fmt.Sprintf("Improper Input Size, %d", len(input)))
+	// }
 
-	var params []float32 = unsafe.Slice((*float32)(unsafe.Pointer(&input[0])), len(input)/4)
+	// var params []float32 = unsafe.Slice((*float32)(unsafe.Pointer(&input[0])), len(input)/4)
 
-	var sampleTime float32 = params[0]
-	var freqHz float32 = params[1]
-	var currPhase float32 = params[2]
+	// var sampleTime float32 = params[0]
+	// var freqHz float32 = params[1]
+	// var currPhase float32 = params[2]
 
 	var numSamples uint32 = uint32(*(*int32)(unsafe.Pointer(&input[3*4])))
 
-	var inputOne float32 = params[4]
-	var inputTwo float32 = params[5]
-	var inputThree float32 = params[6]
-	var inputFour float32 = params[7]
+	// var inputOne float32 = params[4]
+	// var inputTwo float32 = params[5]
+	// var inputThree float32 = params[6]
+	// var inputFour float32 = params[7]
 
-	var isLeftChannel bool = *(*int32)(unsafe.Pointer(&input[8*4])) > 0
+	// var isLeftChannel bool = *(*int32)(unsafe.Pointer(&input[8*4])) > 0
 
-	sample := *(*int64)(unsafe.Pointer(&input[9*4]))
-	sampleRate := params[11]
+	// sample := *(*int64)(unsafe.Pointer(&input[9*4]))
+	// sampleRate := params[11]
 
-	var args WaveArgs = WaveArgs{
-		FreqHz:        freqHz,
-		Inputs:        []float32{inputOne, inputTwo, inputThree, inputFour},
-		Phase:         currPhase,
-		IsLeftChannel: isLeftChannel,
-		Sample:        sample,
-		SampleRate:    sampleRate,
-	}
+	// var args WaveArgs = WaveArgs{
+	// 	FreqHz:        freqHz,
+	// 	Inputs:        []float32{inputOne, inputTwo, inputThree, inputFour},
+	// 	Phase:         currPhase,
+	// 	IsLeftChannel: isLeftChannel,
+	// 	Sample:        sample,
+	// 	SampleRate:    sampleRate,
+	// }
 
 	outBuf := make([]float32, numSamples)
 
-	var noise_vals []float32 = make([]float32, 6)
+	noise_vals := make([]float32, 6)
 
 	// pdk.Log(pdk.LogError, "goop")
 	for sampleNo := 0; sampleNo < len(outBuf); sampleNo++ {
-		args.Phase = float32(math.Mod(float64(currPhase+(args.FreqHz*sampleTime*float32(sampleNo))), 1.0))
-		args.Sample = args.Sample + 1
 		outBuf[sampleNo] = 0.0
 		for noise_index := 0; noise_index < len(noise_vals); noise_index++ {
-			if (sampleNo%noise_index + 1) == 0 {
-				noise_vals[sampleNo] = rand.Float32() // generate new noise sample
+			if (sampleNo % (noise_index + 1)) == 0 {
+				noise_vals[noise_index] = (rand.Float32() * 20.0) - 10.0 // generate new noise sample
 			}
 			outBuf[sampleNo] += noise_vals[noise_index]
 		}
-		outBuf[sampleNo] /= float32(len(noise_vals))
+		outBuf[sampleNo] /= float32(len(noise_vals) + 1)
 	}
 
 	pdk.Output(unsafe.Slice((*byte)(unsafe.Pointer(&outBuf[0])), len(outBuf)*4))
