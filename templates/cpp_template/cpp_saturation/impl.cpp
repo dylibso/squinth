e13a4032f505becc644cb49ac71fb350d0b4ebe2
dp-mason <davidpattenmason@gmail.com>
2025-05-26 14:33:09 -0600

    non-functional sketch of shockly diode equation

--- templates/cpp_template/cpp_saturation/impl.cpp
+++ templates/cpp_template/cpp_saturation/impl.cpp
@@ -31,5 +31,16 @@ std::expected<float, pdk::Error> impl::wave(pdk::WaveArgs &&input) {
   const float itpl_wv = ((1.0 - ws_frac) * wave_vals[ws_index]) +
                         (ws_frac * wave_vals[(ws_index + 1) % 4]);
 
-  return itpl_wv * 10.0;
+  // https://en.wikipedia.org/wiki/Shockley_diode_equation
+  const float scale_current = 0.105;
+  const float diode_voltage = 0.1 * itpl_wv;
+  const float tvolt = 0.0253;
+  const float ifact = 1.68;
+  const float e = 2.7183;
+  const float diode_current =
+      scale_current * (std::pow(e, diode_voltage / (ifact * tvolt)) - 1.0);
+
+  const float mix_factor = input.inputs[1] + 10 * (1.0 / 20.0);
+
+  return ((1.0 - mix_factor) * itpl_wv + diode_current * mix_factor) * 10.0;
 }
